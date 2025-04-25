d3662d085ffadbb4c497fc899f37e2a14e8f90a9
Franz Pierre Ladines <franz.pierre.ladines@adamson.edu.ph>
2025-04-25 20:06:08 +0800

    commit

--- JobPortal/Controllers/HomeController.cs
+++ JobPortal/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace JobPortal.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index() //dadsa
         {
             return View();
         }
